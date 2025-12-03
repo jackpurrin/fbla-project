dadc890660e3265c3dc266e7efd23e8d8a589ef9
~autumn <jackpurrin@tutamail.com>
2025-12-03 18:22:52 +0000

    adds some choice logic

--- python/main.py
+++ python/main.py
@@ -8,27 +8,31 @@ happiness = 50
 money = 1000
 sleeping = False
 
-while (True) :
-    # Prints the current time and hunger using formatted strings
-    print(f"Time is : {igt}")
-    print(f"My hunger is: {hunger}")
+cat = """\
+      \    //
+       )  ( ')
+      (  /  )
+       \(__)|
+"""
 
-    time.sleep(1)
-    # Passes the time
-    igt = igt + 1
-    # Clears the terminal to make it look nice.
-    os.system('cls' if os.name == 'nt' else 'clear')
-    # Reset the day after 24 hours
-    if (igt >= 24):
-        igt = 0
-    # Sleeps from 12 - 8
-    if (igt <= 8) :
-        sleeping = True
-        print("Sleeping.")
-    else :
-        sleeping = False
-        print("Awake.")
-    # Decreases hunger and happiness when awake
-    if (sleeping == False):
-        happiness -= .5
-        hunger -= 1
+dog = """\
+           __
+      (___()'`;
+      /,    /`
+      //"--//
+
+"""
+
+os.system('cls' if os.name == 'nt' else 'clear')
+
+print("Which do you want? A cat or a dog?")
+
+pet = input()
+
+if pet == "cat":
+    print(cat)
+elif pet == "dog":
+    print(dog)
+else: 
+    print(seal)
+
